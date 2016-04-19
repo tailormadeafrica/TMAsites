c72c05e8fb12bfda74cc1d85ea8f2601aa520cf1
unknown <dewald@perfectcircle.co.za>
2016-04-19 13:11:45 +0200

    ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def check_location(location)
+    childs = Refinery::Locations::Location.where(:parent_id => location)
+    if childs.present?
+      true
+    end
+  end
 end
