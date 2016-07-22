9bd9d93eb3113bf2112f703b9fbb2b9db8c283d8
Nick Broekman <nick@perfectcircle.co.za>
2016-07-22 11:00:20 +0200

    , :per_page => 10000

--- vendor/extensions/accommodations/app/controllers/refinery/accommodations/admin/accommodations_controller.rb
+++ vendor/extensions/accommodations/app/controllers/refinery/accommodations/admin/accommodations_controller.rb
@@ -4,7 +4,7 @@ module Refinery
       class AccommodationsController < ::Refinery::AdminController
 
         crudify :'refinery/accommodations/accommodation',
-                :title_attribute => 'name', :xhr_paging => true
+                :title_attribute => 'name', :xhr_paging => true, :per_page => 10000
 
       end
     end
--- vendor/extensions/locations/app/controllers/refinery/locations/admin/locations_controller.rb
+++ vendor/extensions/locations/app/controllers/refinery/locations/admin/locations_controller.rb
@@ -4,7 +4,7 @@ module Refinery
       class LocationsController < ::Refinery::AdminController
 
         crudify :'refinery/locations/location',
-                :title_attribute => 'name', :xhr_paging => true, :order => "lft", :per_page => 1000
+                :title_attribute => 'name', :xhr_paging => true, :order => "lft", :per_page => 10000
 
       end
     end
