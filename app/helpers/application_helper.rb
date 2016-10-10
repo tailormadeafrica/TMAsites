af1b5d8b400cc9560ac33be5227d66537a1a9a34
unknown <dewald@perfectcircle.co.za>
2016-10-10 10:27:18 +0200

    breadcrums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,32 @@ module ApplicationHelper
       true
     end
   end
+
+  def breadcrumbs(page=nil,location=nil,post=nil)
+    links = []
+    links << link_to('Home', '/')
+
+
+    if page.present?
+      if page.link_url.present? and ['/locations'].include? page.link_url
+        links << page.title
+      else
+        links << link_to(page.title, refinery.url_for(page))
+      end
+
+    end
+    if location.present?
+      if location.parent.present?
+        if location.parent.parent.present?
+          links << link_to(location.parent.parent.name, refinery.locations_location_path(location.parent.parent))
+        end
+        links << link_to(location.parent.name, refinery.locations_location_path(location.parent))
+      end
+      links << link_to(location.name, refinery.locations_location_path(location))
+    end
+    if post.present?
+      links << link_to(post.title, refinery.blog_post_url(post))
+    end
+    raw links.join('&nbsp;&nbsp;>&nbsp;&nbsp;')
+  end
 end
