ec01be74a63ca38ef22223af196c4fc1e002a98f
Ronelle <ronelle@perfectcircle.co.za>
2017-07-19 15:53:51 +0200

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -274,6 +274,40 @@ $(document).ready(function() {
         return false
     });
 
+    var showChar = 250;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span><a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+
     ///////////////////////////////////////////
 
     WaterMark('input#inquiry_name','Name');
@@ -313,6 +347,42 @@ $(window).load(function() {
     });
 });
 
-// new UISearch( document.getElementById( 'sb-search' ) );
+
+// $(document).ready(function() {
+//     // Configure/customize these variables.
+//     var showChar = 100;  // How many characters are shown by default
+//     var ellipsestext = "...";
+//     var moretext = "Show more >";
+//     var lesstext = "Show less";
+    
+
+//     $('.more').each(function() {
+//         var content = $(this).html();
+ 
+//         if(content.length > showChar) {
+ 
+//             var c = content.substr(0, showChar);
+//             var h = content.substr(showChar, content.length - showChar);
+ 
+//             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+//             $(this).html(html);
+//         }
+ 
+//     });
+ 
+//     $(".morelink").click(function(){
+//         if($(this).hasClass("less")) {
+//             $(this).removeClass("less");
+//             $(this).html(moretext);
+//         } else {
+//             $(this).addClass("less");
+//             $(this).html(lesstext);
+//         }
+//         $(this).parent().prev().toggle();
+//         $(this).prev().toggle();
+//         return false;
+//     });
+// });
 
 
