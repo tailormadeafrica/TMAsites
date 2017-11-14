8dd57deaee2f2f316b9f635dc475379bbb70581f
Ronelle <ronelle@perfectcircle.co.za>
2017-11-14 13:02:16 +0200

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -353,41 +353,30 @@ $(window).load(function() {
 });
 
 
-// $(document).ready(function() {
-//     // Configure/customize these variables.
-//     var showChar = 100;  // How many characters are shown by default
-//     var ellipsestext = "...";
-//     var moretext = "Show more >";
-//     var lesstext = "Show less";
-    
+// Get the modal
+var modal = document.getElementById('myModal');
 
-//     $('.more').each(function() {
-//         var content = $(this).html();
- 
-//         if(content.length > showChar) {
- 
-//             var c = content.substr(0, showChar);
-//             var h = content.substr(showChar, content.length - showChar);
- 
-//             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
-//             $(this).html(html);
-//         }
- 
-//     });
- 
-//     $(".morelink").click(function(){
-//         if($(this).hasClass("less")) {
-//             $(this).removeClass("less");
-//             $(this).html(moretext);
-//         } else {
-//             $(this).addClass("less");
-//             $(this).html(lesstext);
-//         }
-//         $(this).parent().prev().toggle();
-//         $(this).prev().toggle();
-//         return false;
-//     });
-// });
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
