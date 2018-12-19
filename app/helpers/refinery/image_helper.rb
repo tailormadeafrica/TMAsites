16cb5ee97838c2ac2cc9cd6f7bb9835d205677ad
Gerard Mopp <gerardmopp@gmail.com>
2018-12-19 22:37:00 +0200

    testing compresstion

--- app/helpers/refinery/image_helper.rb
+++ app/helpers/refinery/image_helper.rb
@@ -28,7 +28,7 @@ module Refinery
       if image.present?
         dimensions = (image.thumbnail_dimensions(geometry) rescue {})
 
-        coverted_image = image.thumbnail(geometry).encode("#{image.image_mime_type.split('/').last}", '-strip -quality 55')
+        coverted_image = image.thumbnail(geometry).encode("#{image.image_mime_type.split('/').last}", '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%')
 
         image_tag(coverted_image.url, {
           :alt => image.respond_to?(:title) ? image.title : image.image_name,
