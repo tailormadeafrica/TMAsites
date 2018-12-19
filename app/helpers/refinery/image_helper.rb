0cb43d79b15395ee1321be7cac8d61ab8d569891
Gerard Mopp <gerardmopp@gmail.com>
2018-12-19 22:43:30 +0200

    using guid lines

--- app/helpers/refinery/image_helper.rb
+++ app/helpers/refinery/image_helper.rb
@@ -28,7 +28,7 @@ module Refinery
       if image.present?
         dimensions = (image.thumbnail_dimensions(geometry) rescue {})
 
-        coverted_image = image.thumbnail(geometry).encode("#{image.image_mime_type.split('/').last}", '-strip -interlace Plane -gaussian-blur 0.05 -quality 85%')
+        coverted_image = image.thumbnail(geometry).encode("#{image.image_mime_type.split('/').last}", '-sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB')
 
         image_tag(coverted_image.url, {
           :alt => image.respond_to?(:title) ? image.title : image.image_name,
