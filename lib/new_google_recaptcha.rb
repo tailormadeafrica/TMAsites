57ea3b33ce546d45e20efc0c96cadeefd7741f29
Gerard Mopp <gerardmopp@gmail.com>
2019-01-30 21:23:24 +0200

    added google captcha

---
+++ lib/new_google_recaptcha.rb
@@ -0,0 +1,28 @@
+module NewGoogleRecaptcha
+  mattr_accessor :site_key
+  mattr_accessor :secret_key
+  mattr_accessor :minimum_score
+
+  def self.setup
+    yield(self)
+  end
+
+  def self.human?(token, action, minimum_score = self.minimum_score, model = nil)
+    is_valid = NewGoogleRecaptcha::Validator.valid?(token, action, minimum_score)
+    if model && !is_valid
+      model.errors.add(:base, self.i18n("new_google_recaptcha.errors.verification_human", "Looks like you are not a human"))
+    end
+    is_valid
+  end
+
+  def self.i18n(key, default)
+    if defined?(I18n)
+      I18n.translate(key, default: default)
+    else
+      default
+    end
+  end
+
+end
+
+require_relative "new_google_recaptcha/validator"
