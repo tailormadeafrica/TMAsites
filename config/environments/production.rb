320694e84ce03ecfbdf7700962adfba44ede8706
Gerard Mopp <gerardmopp@gmail.com>
2019-03-28 09:42:49 +0200

    remove edge for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ TailorMadeAfrica::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = ENV['EDGE_URL']
+  # config.action_controller.asset_host = ENV['EDGE_URL']
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
