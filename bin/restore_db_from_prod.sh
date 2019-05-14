836ca5bb29ab5948fad40996a79425f830d7581b
Gerard Mopp <gerardmopp@gmail.com>
2019-05-14 20:38:53 +0200

    typo

--- bin/restore_db_from_prod.sh
+++ bin/restore_db_from_prod.sh
@@ -2,6 +2,6 @@
 
 heroku pg:backups capture
 curl -o /tmp/latest.dump `heroku pg:backups public-url -r heroku`
-PGPASSWORD=tma psql -h localhost -p 5432 -U tma -d tma_dev -c "DROP DATABASE tma;"
-PGPASSWORD=tma psql -h localhost -p 5432 -U tma -d tma_dev -c "CREATE DATABASE tma:"
+PGPASSWORD=tma psql -h localhost -p 5432 -U tma -d tma_dev -c "DROP DATABASE tma_dev;"
+PGPASSWORD=tma psql -h localhost -p 5432 -U tma -d tma_dev -c "CREATE DATABASE tma_dev;"
 PGPASSWORD=tma pg_restore --verbose --clean --no-acl --no-owner -h localhost -U tma -d tma_dev /tmp/latest.dump
